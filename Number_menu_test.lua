f184b4e784d15c9effc21589bf683f19a4064b34
jsm0722 <64713508+jsm0722@users.noreply.github.com>
2022-08-26 10:14:20 -0500

    Create Number_menu_test.lua

---
+++ Number_menu_test.lua
@@ -0,0 +1,16 @@
+shell=get_shell
+computer=shell.host_computer
+
+program_one=function()
+  print("Program One")
+ end function
+ 
+ 
+program_two=function()
+  print("Program Two")
+end function
+ 
+ 
+ program_three=function()
+  print("Program Three")
+ end function
